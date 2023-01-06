fcd897542d17584b5b80fb7db9c12506bf2f56fb
imabhishekmahli <abhi8h3k@gmail.com>
2023-01-06 12:53:35 +0530

    more on if else

---
+++ 02.Basics/moreifelse.js
@@ -0,0 +1,9 @@
+let a = 'staff'
+
+if(a=='student'){
+    console.log('this is for student')
+} else if(a=='teacher'){
+    console.log('this is for teacher')
+} else if(a=='staff'){
+    console.log('this is for staff')
+}
