fcecd7b71432a03edd19ff2f31bb4a48e148db7c
imabhishekmahli <abhi8h3k@gmail.com>
2023-01-06 18:16:19 +0530

    login logic

---
+++ 02.Basics/loginifelse.js
@@ -0,0 +1,12 @@
+var whoishere = 'hie'
+
+if(whoishere == 'user'){
+    console.log('greeting message for user')
+    console.log('Allow access to view all courses')
+} else if(whoishere == 'teacher'){
+    console.log('greeting message for teacher')
+    console.log('Allow access to his courses')
+} else{
+    console.log('please verify your mail')
+    console.log('send user an email for verification')
+}
