5824113f94d140ff180ae22ded75a1c765a3f47c
imabhishekmahli <abhi8h3k@gmail.com>
2023-01-06 18:24:02 +0530

    school grade problem

---
+++ 02.Basics/GradeProblem.js
@@ -0,0 +1,11 @@
+var marks = 5
+
+if(marks == 10){
+    console.log('Amazing')
+} else if(marks == 5){
+    console.log('Good')
+} else if(marks == 3){
+    console.log('Poor')
+} else{
+    console.log('Fail')
+}
