62a46c366cd8f5d41692d7acaa45845f85b0da0c
imabhishekmahli <abhi8h3k@gmail.com>
2023-01-10 17:03:36 +0530

    calculate Discount using operatos

--- 02.Basics/operators.js
+++ 02.Basics/operators.js
@@ -1,4 +1,17 @@
-var num1 = 69;
-var num2 = 69;
+// var num1 = 69;
+// var num2 = 69;
 
-console.log(num1 + num2)
+// console.log(num1 + num2)
+
+
+// D = (L - S)/L * 100
+
+var sellingPrice = 199;
+var listingPrice = 799;
+
+var discountPercent = ((listingPrice - sellingPrice) / listingPrice) * 100;
+console.log("Discount Percentage is :",discountPercent)
+
+displayDiscountPercentage = Math.round(discountPercent);
+
+console.log(displayDiscountPercentage + "% off")
