ae2d255bcb57f456af4e02651635bdd17dc3b837
imabhishekmahli <abhi8h3k@gmail.com>
2023-01-10 17:18:22 +0530

    comparing listingPrice and sellingPrice

---
+++ 02.Basics/typesAndOperatorPrecedence.js
@@ -0,0 +1,13 @@
+var sellingPrice = 199;
+var listingPrice = 799;
+
+var discountPercent = ((listingPrice - sellingPrice) / listingPrice) * 100;
+console.log("Discount Percentage is :",discountPercent)
+
+displayDiscountPercentage = Math.round(discountPercent);
+
+console.log(displayDiscountPercentage + "% off")
+
+var results = listingPrice > sellingPrice
+
+console.log(results)
