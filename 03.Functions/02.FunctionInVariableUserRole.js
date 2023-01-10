4dbb712a30c4eca9c3968a398ad3b217ca4234f4
imabhishekmahli <abhi8h3k@gmail.com>
2023-01-10 19:49:18 +0530

    user role using FUNCTION

---
+++ 03.Functions/02.FunctionInVariableUserRole.js
@@ -0,0 +1,22 @@
+function getUserRole(name , role){
+    switch(role){
+        case "admin":
+            return `${name} is admin with all access`
+            break;
+        case "subadmin":
+            return `${name} is sub-admin with all access`
+            break;
+        case "testprep":
+            return `${name} is testprep with access to create and delete courses`
+            break;
+        case "user":
+            return `${name} is user and have access to the content `
+            break;
+
+        default:
+            return `${name} is a trial user`
+            break;
+    }
+}
+
+console.log(getUserRole('abhishek','user'))
