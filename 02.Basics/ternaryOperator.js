1bc129106e3881f1853cad880860a8c5f73dd68d
imabhishekmahli <abhi8h3k@gmail.com>
2023-01-10 17:50:47 +0530

    another way of ifelse

--- 02.Basics/ternaryOperator.js
+++ 02.Basics/ternaryOperator.js
@@ -1,7 +1,9 @@
 var authenticated = false
 
-if(authenticated){
-    console.log('show signout button')
-} else{
-    console.log('show login button')
-}
+// if(authenticated){
+//     console.log('show signout button')
+// } else{
+//     console.log('show login button')
+// }
+
+authenticated ? console.log('login') : console.log("sign out")
