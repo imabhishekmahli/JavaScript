dc0c3a7b391e3d07d7da2f7a179d2ebca872bf5c
imabhishekmahli <abhi8h3k@gmail.com>
2023-01-28 23:31:07 +0530

    you can not change const object

--- practiceSet1/practice1.js
+++ practiceSet1/practice1.js
@@ -5,3 +5,11 @@ console.log(a+" ",b)
 
 //Q2. use typeof operator to find the datatype of string in the last question>
 console.log(typeof(a+b))
+
+//Q3. create a const object in JS. can you change it to hold a number later?
+const x = {
+    name : 'abhishek',
+    age : 25
+}
+x = 35
+console.log(x)
