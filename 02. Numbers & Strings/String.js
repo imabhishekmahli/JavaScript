0b7ee35a99cb075027c050abfc1f66091eaa7929
imabhishekmahli <abhi8h3k@gmail.com>
2024-06-02 18:06:48 +0530

    bill amount with 18% GST

--- 02. Numbers & Strings/String.js
+++ 02. Numbers & Strings/String.js
@@ -6,3 +6,14 @@ let iceCream = prompt("enter icecream quantities :")
 
 let billAmount = (dal*100) + (roti*10) + (iceCream*30)
 alert(billAmount)
+
+
+// Q2. Calculate the amount with 18% GST 
+
+let dal = prompt("Enter dal quantities :")
+let roti = prompt("enter roti quantities :")
+let iceCream = prompt("enter icecream quantities :")
+
+let billAmount = (dal*100) + (roti*10) + (iceCream*30) + (18/100)*(dal*100) + (roti*10) + (iceCream*30)
+alert(billAmount)
+
