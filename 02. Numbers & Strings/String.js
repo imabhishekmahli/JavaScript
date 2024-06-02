5f998e05fa7a850938d3ff895c07ac634ddd8c91
imabhishekmahli <abhi8h3k@gmail.com>
2024-06-02 17:57:50 +0530

    took input form user and displayed the result

---
+++ 02. Numbers & Strings/String.js
@@ -0,0 +1,8 @@
+// Q1. At restaurant you eat something display final bill amount 
+
+let dal = prompt("Enter dal quantities :")
+let roti = prompt("enter roti quantities :")
+let iceCream = prompt("enter icecream quantities :")
+
+let billAmount = (dal*100) + (roti*10) + (iceCream*30)
+alert(billAmount)
